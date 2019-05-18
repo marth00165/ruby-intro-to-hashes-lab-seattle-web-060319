8d2c6700c195849271dd6f91ebc40e2cbc3cd7c3
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-18 01:48:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -71,7 +71,9 @@ def monopoly_with_fourth_tier
 	    :pieces => 4,
 	    :name => {
 	      
-	      :reading_railroad => {},
+	      :reading_railroad => {
+	        "mortga"
+	      },
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
 	      :shortline => {}
